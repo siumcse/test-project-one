a5672ca89b8e26ec2fec7132e65eb362d834a35b
Sium <siumcse@gmail.com>
2024-07-13 16:52:28 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: gray;
+        }
+        p{
+            text-align: justify;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor, ullam saepe laudantium reiciendis accusantium.</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptatem minus ipsum optio! Suscipit ut veniam beatae reprehenderit, enim dolore eum soluta cupiditate saepe sit minima sint quia obcaecati repudiandae neque facilis! Repellat earum impedit commodi dolor explicabo nihil id placeat exercitationem sed, quos ipsum? Vero veniam enim tenetur, ut velit sit debitis nulla ipsum aliquid voluptas! Quas quia, ea doloribus praesentium id accusamus debitis esse assumenda ducimus dolorem nisi necessitatibus doloremque neque tempora ut nemo. Praesentium autem error doloremque nulla. Deleniti vel placeat ea ex quaerat ipsam quibusdam non, rem delectus sint voluptate, dicta porro labore blanditiis consectetur culpa.</p>
+
+    <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit aliquid officiis culpa, nobis distinctio debitis.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae magni consequatur iste numquam corrupti accusantium reprehenderit, ab fugit quam deleniti iure inventore quia rem nisi reiciendis distinctio impedit. Soluta molestiae optio ab odio, minus assumenda? Explicabo laborum recusandae optio dolor maxime error. Ad, porro alias! Quisquam debitis facilis, excepturi esse, ipsum quis repellat illo obcaecati necessitatibus numquam rem provident repudiandae ut incidunt. Maxime animi voluptates velit incidunt dolorum similique, consequatur nulla autem culpa minima modi, expedita repellat tenetur, odio unde optio aperiam ullam ab voluptatibus! Doloremque provident similique odit magnam accusamus reiciendis placeat labore ipsam suscipit? Excepturi, ea ex.</p>
+
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil accusamus vitae, dolorem saepe eaque sapiente.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis possimus ad ipsa voluptatem placeat neque. Dolores ipsum maiores est fugit sed a iusto recusandae voluptates natus molestias itaque impedit consequuntur nemo alias consequatur dignissimos, sint vitae. Quod ratione dolorum qui ex assumenda adipisci tenetur incidunt voluptates, optio omnis consectetur autem corporis dignissimos, recusandae debitis nemo aut quis placeat doloribus rerum expedita iste beatae velit voluptatibus! Officiis magni ratione temporibus repudiandae mollitia praesentium molestiae deserunt error odio consequuntur aperiam cupiditate non id modi totam officia ipsum adipisci facilis eveniet omnis obcaecati, nihil dolores incidunt fugit? Quisquam quod eum fugiat impedit aliquid!</p>
+
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error soluta dolores quis?</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, inventore ipsum voluptates numquam quam iure aspernatur? Distinctio, ab placeat quisquam nihil ipsum eaque provident. Libero, quo. Perspiciatis eaque rerum voluptates similique voluptate quaerat ullam ratione esse soluta dicta nostrum harum quos, ipsa natus voluptatem vero aperiam aliquam ducimus tempore eligendi incidunt pariatur consequuntur totam? Perferendis ratione earum aliquam nihil vero eum nam facere quis quae in reprehenderit ut, aut pariatur similique cumque ab, rerum dolorum eos explicabo quas aspernatur ex. Beatae molestiae quisquam maxime repellat nam id esse, minima maiores.</p>
+    
+</body>
+</html>
